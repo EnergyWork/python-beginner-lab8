761f1bcb88123955961d830ea01d37066e6be6a9
Artyom <ag.energy@mail.ru>
2020-10-25 17:34:18 +0300

    update

--- task1/SongClass.py
+++ task1/SongClass.py
@@ -3,9 +3,19 @@ class Song(object):
     '''Класс, описывающий композицию'''
     def __init__(self):
         self._title = None
+        self._author = None
+        self._album = None
+        self._year_out = None
+        self._duration = None
     @property
     def title(self):
         return self._title
     @title.setter
     def title(self, value):
         self._title = value
+    @property
+    def author(self):
+        return self._author
+    @author.setter
+    def author(self, value):
+        self._author = value
